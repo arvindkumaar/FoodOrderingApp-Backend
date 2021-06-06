bfb8f6a75d61afb8842309429930bfd454a4ae63
arvindkumaar <arvind.tomar@gmail.com>
2021-06-06 12:36:07 +0530

    Get payment methods endpoint

--- FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/PaymentService.java
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/PaymentService.java
@@ -18,14 +18,4 @@ public class PaymentService {
     public List<PaymentEntity> getAllPaymentMethods() {
         return paymentDao.getAllPaymentMethods();
     }
-
-    @Transactional
-    public PaymentEntity getPaymentById(final Long paymentId) {
-        return paymentDao.getPaymentById(paymentId);
-    }
-
-    @Transactional
-    public PaymentEntity getPaymentByUuid(final String paymentUuid) {
-        return paymentDao.getPaymentByUuid(paymentUuid);
-    }
 }
